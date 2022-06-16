1a9136651660adba8c692c52333ddfda464bed54
smallg <smallg1015@gmail.com>
2022-06-16 15:28:19 +0800

    更新header及banner RWD

--- all.js
+++ all.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
-    $('.home-banner-content').slick({
-    });
-  });
+let showmenu = document.querySelector('.showmenu');
+let header_nav_main = document.querySelector('.header-nav-main');
+showmenu.addEventListener('click',function(){
+    if(!header_nav_main.classList.contains('menu-show')){
+        header_nav_main.classList.add('menu-show');
+    }else{
+        header_nav_main.classList.remove('menu-show');
+    }
+})
