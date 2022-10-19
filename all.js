fc4fd24d20d8928c269d524f9a80ec1540fdff15
smallg <smallg1015@gmail.com>
2022-10-19 16:48:23 +0800

    寄信測試2

--- all.js
+++ all.js
@@ -13,7 +13,7 @@ function sendEmail(){
     Email.send({
         SecureToken : "af9f143a-aa7b-4ebd-a67a-e36c5def447f",
         To : 'lovebian1103@gmail.com',
-        From : "UMAX<umax@umax.tw>",
+        From : "UMAX<smallg1015@gmail.com>",
         Subject : "This is the subject",
         Body : "And this is the body"
     }).then(
