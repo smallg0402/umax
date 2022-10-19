0ba4a3df84e7305a294019379858cba4756606f5
smallg <smallg1015@gmail.com>
2022-10-19 16:37:00 +0800

    測試寄信功能

--- all.js
+++ all.js
@@ -1,3 +1,4 @@
+
 let showmenu = document.querySelector('.showmenu');
 let header_nav_main = document.querySelector('.header-nav-main');
 showmenu.addEventListener('click',function(){
@@ -7,4 +8,17 @@ showmenu.addEventListener('click',function(){
         header_nav_main.classList.remove('menu-show');
     }
 })
+
+function sendEmail(){
+    Email.send({
+        SecureToken : "af9f143a-aa7b-4ebd-a67a-e36c5def447f",
+        To : 'lovebian1103@gmail.com',
+        From : "UMAX<umax@umax.tw>",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
 
